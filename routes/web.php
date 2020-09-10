0cb24eb70f97dfd42dcee4a16aea77ca0fb64051
testinggit <https://github.com/NangHaungNwan/testinggit.git>
2020-09-10 13:52:06 +0630

    web.php/nhn

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
 Route::get ('/testing',function($value=''){
 	return 'This is a testing page!';
 });
+
+Route::get ('/test',function($value=''){
+	return 'This is a test page!';
+});
+
