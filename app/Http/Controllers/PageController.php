ee37ea655c27351b8a9d17e532f5642d30daa7e2
testinggit <https://github.com/NangHaungNwan/testinggit.git>
2020-09-11 14:43:18 +0630

    cut frontend.blade.php

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function mainfun($value='')
+     {
+
+     	return view('frontend.main');
+     }
+}
